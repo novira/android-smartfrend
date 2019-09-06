8348a2f6a671b620c618ec00d8f881a9d9e71e9f
novira <31467826+novira@users.noreply.github.com>
2019-09-06 07:31:19 +0700

    Add files via upload

---
+++ testing/Editor/noplaying.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+public class noplaying {
+
+	[Test]
+	public IEnumerator nowplay() {
+		string key = "aa9ba31a0fc74a076849181a5b323377";
+		UnityWebRequest www = UnityWebRequest.Get("https://api.themoviedb.org/3/movie/now_playing?api_key="+key+"&language=en-US&page=1");
+		yield return www.Send();
+		Assert.IsTrue (www.isDone);
+	}
+}
---
+++ testing/Editor/poppeople.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+public class poppeople {
+
+	[Test]
+	public IEnumerator popmovie() {
+		string key = "aa9ba31a0fc74a076849181a5b323377";
+		UnityWebRequest www = UnityWebRequest.Get("https://api.themoviedb.org/3/person/popular?api_key="+key+"&language=en-US&page=1");
+		yield return www.Send();
+		Assert.IsTrue (www.isDone);
+	}
+}
